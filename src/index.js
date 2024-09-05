3a9fbd06d53a0e38d621a88e9acbb1bbad9ea3f9
ax1lebafer <raul8917@gmail.com>
2024-09-06 00:44:57 +0400

    Доделал обработку запросов

--- src/index.js
+++ src/index.js
@@ -1,40 +1,51 @@
 const http = require("http");
 const getUsers = require("./modules/users");
 
-const server = http.createServer((request, response) => {
-  // Написать обработчик запроса:
-  // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-  // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-  // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-  // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-  // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
-  const url = new URL(request.url, "http://127.0.0.1");
-  console.log(url);
-  console.log(url.searchParams);
+const PORT = 3003;
+const HOST = "http://127.0.0.1";
 
-  if (request.url === "/hello") {
-    response.statusCode = 400;
-    response.statusMessage = "Bad Request";
-    response.header = "Content-Type: text/plain";
-    response.write("Enter a name");
+const server = http.createServer((request, response) => {
+  const url = new URL(request.url, HOST);
+
+  if (url.searchParams.has("hello")) {
+    const name = url.searchParams.get("hello");
+
+    if (name) {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.write(`Hello, ${name}.`);
+    } else {
+      response.statusCode = 400;
+      response.setHeader("Content-Type", "text/plain");
+      response.write("Enter a name");
+    }
     response.end();
+    return;
   }
 
-  if (request.url === "/users") {
+  if (url.searchParams.has("users")) {
     response.statusCode = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+    response.setHeader("Content-Type", "application/json");
     response.write(getUsers());
     response.end();
+    return;
   }
 
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
-  response.write("Hello world!");
+  if (url.search === "") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "text/plain");
+    response.write("Hello world!");
+    response.end();
+    return;
+  }
+
+  response.statusCode = 500;
+  response.setHeader("Content-Type", "text/plain");
+  response.write("");
   response.end();
 });
 
-server.listen(3003, () => {
-  console.log("Сервер запущен на порту 3003");
+server.listen(PORT, () => {
+  console.log(`Сервер запущен по адресу: ${HOST}:${PORT}`);
 });
